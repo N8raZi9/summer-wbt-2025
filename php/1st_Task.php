f08db0f96958b313670423f16df6bcaaa81a5a3e
N8raZi9 <istiakahmad003@gmail.com>
2025-08-26 23:22:51 +0600

    22-47389-2

---
+++ php/1st_Task.php
@@ -0,0 +1,67 @@
+
+
+<?php
+// ..............................ASSESSMENT TASK 1 ...........................................
+
+// echo"ASSESSMENT TASK 1 <br>";
+
+// $length = 7;
+// $width = 5;
+// $area = $length * $width;
+// $parimeter = 2 * ($length + $width);
+ 
+// echo "Length: " . $length . " <br>";
+// echo "Width: " . $width . " <br>";
+// echo "Area of Rectangle =  $area <br>";
+// echo "Perimeter of Rectangle = $parimeter <br>";
+ 
+ 
+// ..............................ASSESSMENT TASK 2 ...........................................
+
+// echo"ASSESSMENT TASK 2 <br>";
+
+// $price = 70;
+// $total = $price * (15 / 100);
+// $totalvatprice = $total + $price;
+// echo "Total VAT added price is  = $totalvatprice <br>";
+ 
+ 
+ 
+// ..............................ASSESSMENT TASK 3 ...........................................
+ 
+// echo"ASSESSMENT TASK 3 <br>";
+
+// $x = 70;
+// if ($x % 2 == 0) {
+//     echo " this number is even $x. <br>";
+// } else {
+//     echo "this number is odd $x.";
+// }
+ 
+// ..............................ASSESSMENT TASK 4 ...........................................
+
+// echo"ASSESSMENT TASK 4 <br>";
+
+// $x = 30;
+// $y = 40;
+// $z = 50;
+ 
+// if ($x >= $y && $x >= $z) {
+//     echo "the largest number is $x";
+// } elseif ($y >= $x && $y >= $z) {
+//     echo "the largest number is $y";
+// } else {
+//     echo "the largest number is $z";
+// }
+ 
+// ..............................ASSESSMENT TASK 5 ...........................................
+ 
+// echo"ASSESSMENT TASK 5 <br>"; 
+
+// for ($x = 10; $x < 100; $x++) {
+//     if ($x % 2 == 1) {
+//         echo " $x";
+//     }
+// }
+
+?>
